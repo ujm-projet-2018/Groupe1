808112c2b8b14d6869c2584e43de4278cc3e03f0
abdel42 <32333972+abdel42@users.noreply.github.com>
2018-04-21 12:12:08 +0200

    Ajout de mot clé

--- principal/SQL.java
+++ principal/SQL.java
@@ -1,11 +1,7 @@
 package projetTutore;
 
 public interface SQL {
-	/*
-	*pour ne pas faire planteur le comparateur d'arbre
-	*/
 	String SELECT = "SELECT";
-	String AS = "AS";
 	String AGREGAT = "AGREGAT";
 	String FROM = "FROM";
 	String WHERE = "WHERE";
@@ -14,6 +10,6 @@ public interface SQL {
 	String MIN = "MIN";
 	String MAX = "MAX";
 	String SUM = "SUM";
-	
+	String ATTRIBUT = "ATTRIBUT";
 
 }
