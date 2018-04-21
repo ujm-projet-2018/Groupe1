05de07e98495f7562c5ccb669b4dddb135541228
abdel42 <32333972+abdel42@users.noreply.github.com>
2018-04-21 12:10:51 +0200

    Modification regex/ amélioration de l'analyseur

--- principal/AnalyseReponse.java
+++ principal/AnalyseReponse.java
@@ -31,7 +31,7 @@ public class AnalyseReponse extends Arbre{
 		super();
 		this.reponse = reponse;
 	}
- 
+
 
 	public AnalyseReponse(Arbre reponse, String zone) {
 		super();
@@ -83,10 +83,11 @@ public class AnalyseReponse extends Arbre{
 	 * @return vrai si la requete contient des agregats COUNT/AVG/MIN/MAX/SUM
 	 */
 	public boolean agregat(String requete) {
-		String agregats = "( COUNT[(]| AVG( )*[(]| MIN[(]| MAX[(]| SUM[(])";
-		/*
-		*attention, regex a retravailler , truc du genre [,| ]COUNT([(.+)])[,| ]
-		*/
+		String agregats = "[,| |, ]COUNT[(](.+)[)]"
+				+ "|[,| |, ]AVG[ ]*[(](.+)[)]"//il peut y avoir des espaces entre le AVG et sa parenthese, pas pour les autres agregats
+				+ "|[,| |, ]MIN[(](.+)[)]"
+				+ "|[,| |, ]MAX[(](.+)[)]"
+				+ "|[,| |, ]SUM[(](.+)[)]";
 		pattern = Pattern.compile(agregats);
 		matcher = pattern.matcher(requete.toUpperCase());
 		if(matcher.find()) {
@@ -121,23 +122,161 @@ public class AnalyseReponse extends Arbre{
 	 */
 	public void analyseFrom(String requete, Arbre arbre) {
 		String[] attribut;
+
 		arbre.ajouter(SQL.FROM);
-		requete = "T_eleve,T_prof,T_emploi";
-		pattern = Pattern.compile(",");
-		attribut = pattern.split(requete);
-		for(int i=0;i<attribut.length;i++) {
-			System.out.println(attribut[i]);
+		//requete = "T_eleve,T_prof,T_emploi";
+		/*
+		 * on cherche a savoir si il y a un WHERE
+		 */
+		pattern = Pattern.compile(" WHERE ");
+		matcher = pattern.matcher(requete);
+		if(matcher.find()) {
+			System.out.println("Il y a un WHERE");
+			pattern = Pattern.compile("(.+) WHERE(.+)");
+			matcher = pattern.matcher(requete);
+			if(matcher.find()) {
+				String avantWhere,apresWhere;
+				avantWhere=matcher.group(1);
+				apresWhere=matcher.group(2);
+				System.out.println("avantWhere : "+avantWhere+
+						"\n"
+						+ "apresWhere : "+apresWhere);
+
+
+				pattern = Pattern.compile(",");
+				attribut = pattern.split(avantWhere);
+				for(int i=0;i<attribut.length;i++) {
+					System.out.println(attribut[i]);
+				}
+
+				ArrayList<String> attributs = new ArrayList<>();
+				for(int i=0;i<attribut.length;i++) {
+					attributs.add(attribut[i]);
+				}
+				Collections.sort(attributs);
+				arbre.ajouter(attributs.toString());
+				System.out.println("Voila donc les elements du FROM : "+attributs);
+				System.out.println("Nous allons maintenant appeler analyseWhere...");
+				analyseWhere(apresWhere, arbre);
+			}
+		}else {
+			System.out.println("Il n'y a pas de WHERE");
+
+			String avantWhere = requete;
+			pattern = Pattern.compile(",");
+			attribut = pattern.split(avantWhere);
+			for(int i=0;i<attribut.length;i++) {
+				System.out.println(attribut[i]);
+			}
+
+			ArrayList<String> attributs = new ArrayList<>();
+			for(int i=0;i<attribut.length;i++) {
+				attributs.add(attribut[i]);
+			}
+			Collections.sort(attributs);
+			arbre.ajouter(attributs.toString());
+			System.out.println("Voila donc les elements du FROM : "+attributs);
+			System.out.println("Il n'y a pas de where");
+
+			//prendre en charge les GROUP BY, HAVING, ORDER BY
 		}
-		
-		ArrayList<String> attributs = new ArrayList<>();
-		for(int i=0;i<attribut.length;i++) {
-			attributs.add(attribut[i]);
-		}
-		Collections.sort(attributs);
-		arbre.ajouter(attributs.toString());
-		System.out.println("Test tefr"+attributs);
-		// reste a trier la liste, puis la mettre dans l'arbre
 	}
+
+
+
+	public boolean opComparaison(String requete) {
+		String agregats = "[ ]*=[ ]*"
+				+ "|[ ]*<>[ ]*"
+				+ "|[ ]*!=[ ]*"
+				+ "|[ ]*<[ ]*"
+				+ "|[ ]*>[ ]*"
+				+ "|[ ]*>=[ ]*"
+				+ "|[ ]*<=[ ]*"
+				+ "|[ ]*IN[ ]*"
+				+ "|[ ]*BETWEEN[ ]*"
+				+ "|[ ]*LIKE[ ]*"
+				+ "|[ ]*IS NULL[ ]*"
+				+ "|[ ]*IS NOT NULL[ ]*"
+				+ "|[ ]*AND[ ]*"
+				+ "|[ ]*OR[ ]*"
+				+ "|[,| |, ]MAX[(](.+)[)]"
+				+ "|[,| |, ]SUM[(](.+)[)]";
+		pattern = Pattern.compile(agregats);
+		matcher = pattern.matcher(requete);
+		if(matcher.find()) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	public void analyseWhere(String requete, Arbre arbre) {
+		System.out.println("(where) La requete analysée est : "+requete);
+		arbre.ajouter(SQL.WHERE);
+		String tabWhere[];
+		pattern = Pattern.compile("AND");
+		tabWhere = pattern.split(requete);
+		ArrayList<String> arrayAttributs = new ArrayList<>();
+		System.out.println("Les differents attibuts :");
+		for(int i=0;i<tabWhere.length;i++) {
+			System.out.println(tabWhere[i]);
+		}
+		for(int i=0;i<tabWhere.length;i++) {
+			arrayAttributs.add(tabWhere[i]);
+		}
+		/*
+		 * ici on a mis tout les condition du where dans un tableau, maintenant on va separer ce tableau 
+		 * selon si il contient des jointures ou des operateur de comparaison
+		 */
+		ArrayList<String> tabOperateur = new ArrayList<>();
+		Boolean tabOperateurExiste = false;
+		if(opComparaison(requete)) {
+			System.out.println("Il y a un/des operateurs de comparaison");
+			tabOperateurExiste = true;
+			for(int i=0;i<arrayAttributs.size();i++) {
+				System.out.println(arrayAttributs.get(i)+" est analysée");
+				if(opComparaison(arrayAttributs.get(i))){//on ajoute un espace 
+					System.out.println("c'est un operateur");
+					tabOperateur.add(arrayAttributs.get(i));
+					arrayAttributs.remove(i);
+				}
+			}
+		}
+
+		/*
+		 * on a maintenant deux liste :
+		 * (tabMotCle si il y a des agregats) et arrayAttribut
+		 * on va les trier et les ajouter a l'arbre
+		 */
+		Collections.sort(arrayAttributs);
+		if(tabOperateurExiste) {
+			Collections.sort(tabOperateur);
+			//tabMotCle.addAll(arrayAttributs);
+		}
+
+
+		System.out.println("Les tables sont :");
+		System.out.println("tabOperateur : "+tabOperateur);
+		System.out.println("arrayAttribut : "+arrayAttributs);
+
+		System.out.println("Et donc :");
+		if(tabOperateurExiste) {
+		//System.out.println(tabMotCle);
+			arbre.ajouter("WHERE");
+			//decomposition.ajouter(tabMotCle.toString());
+		}
+		System.out.println(arrayAttributs);
+		arbre.ajouter("WHERE");
+		arbre.ajouter(arrayAttributs.toString());
+
+
+		System.out.println(decomposition);
+		System.out.println("apres analyse du where :");
+		//analyseFrom(apresFrom, decomposition);
+		System.out.println(decomposition);
+	}
+
+
 	/**
 	 * methode a applle si on rencontre des agregats
 	 * @param requete
@@ -145,17 +284,22 @@ public class AnalyseReponse extends Arbre{
 	 */
 	public Arbre analyseAgregat(String requete) {
 		Arbre arbre = new Arbre();
-		String agregats = "(,COUNT[(](.+)[)]| COUNT[(.+)]| AVG( )*[(]| MIN[(]| MAX[(]| SUM[(])";
-		requete = " eleve,COUNT(t_eleve),hju";
+		String agregats = "[,| |, ]COUNT[(](.+)[)]"
+				+ "|[,| |, ]AVG[ ]*[(](.+)[)]"//il peut y avoir des espaces entre le AVG et sa parenthese, pas pour les autres agregats
+				+ "|[,| |, ]MIN[(](.+)[)]"
+				+ "|[,| |, ]MAX[(](.+)[)]"
+				+ "|[,| |, ]SUM[(](.+)[)]";
+		requete = "SELECT *,MIN(t_eleve),hju";
 		pattern = Pattern.compile(agregats);
 		matcher = pattern.matcher(requete);
 		if(matcher.find()) {
-			System.out.println("Il y a "+matcher.groupCount());
+			//System.out.println("Il y a "+matcher.groupCount());
 			System.out.println("match");
-			System.out.println(matcher.group());//mettre dans une variable
+			System.out.println(matcher.group());
+			System.out.println(">> "+matcher.group(1));//mettre dans une variable
 			System.out.println("le contenue est : "+matcher.group(2));//dans une autre variables
 		}
-		
+
 		//chercher regex dans variable 1
 		//creer arbre avec variable 1 et 2
 		System.out.println();
@@ -175,14 +319,9 @@ public class AnalyseReponse extends Arbre{
 		Arbre arbre = new Arbre();
 		return arbre;
 	}
-	
-	/*
-	*split le while, detection AS, jointure / operateur / comparateur / mot cle SQL
-	*
-	*/
 	public Arbre decomposeArbre(String requete) {
 		Arbre decomposition = new Arbre();
-		requete = "SELECT t-eleve,al^pha.t-prof, COUNT(rotie)      \n FROM TABLE WHILE eleve = 2";
+		requete = "SELECT t-eleve,alpha.t-prof,COUNT(rotie)      \n FROM TABLE,eleve WHERE eleve = 2 AND prof=7 AND prof.nom=eleve.nom";// WHERE eleve = 2
 		requete = filtre(requete);
 		System.out.println("La requete analysée est : "+requete);
 
@@ -216,17 +355,20 @@ public class AnalyseReponse extends Arbre{
 			ArrayList<String> tabMotCle = new ArrayList<>();
 			Boolean tabMotCleExiste = false;
 			if(agregat(selectFrom) || distinct(selectFrom)) {
+				System.out.println("Il y a un agregat");
 				//il y a des agregat/distinct, on separe la liste en 2 :
 				//une liste de mot cle type SUM, COUNT ... et une liste d'attibut
 				tabMotCleExiste = true;
 				for(int i=0;i<arrayAttributs.size();i++) {
-					if(distinct(arrayAttributs.get(i)) || agregat(arrayAttributs.get(i))){
+					System.out.println(arrayAttributs.get(i)+" est analysée");
+					if(distinct(arrayAttributs.get(i)) || agregat(" "+arrayAttributs.get(i))){//on ajoute un espace 
+						System.out.println("c'est un agregats");
 						tabMotCle.add(arrayAttributs.get(i));
 						arrayAttributs.remove(i);
 					}
 				}
 			}
-			
+
 			/*
 			 * on a maintenant deux liste :
 			 * (tabMotCle si il y a des agregats) et arrayAttribut
@@ -235,38 +377,34 @@ public class AnalyseReponse extends Arbre{
 			Collections.sort(arrayAttributs);
 			if(tabMotCleExiste) {
 				Collections.sort(tabMotCle);
-				tabMotCle.addAll(arrayAttributs);
+				//tabMotCle.addAll(arrayAttributs);
 			}
 
 
 			System.out.println("Les tables sont :");
-			System.out.println(tabMotCle);
-			System.out.println(arrayAttributs);
-			
+			System.out.println("tabMotCle : "+tabMotCle);
+			System.out.println("arrayAttribut : "+arrayAttributs);
+
 			System.out.println("Et donc :");
 			if(tabMotCleExiste) {
 				System.out.println(tabMotCle);
+				decomposition.ajouter(SQL.AGREGAT);
 				decomposition.ajouter(tabMotCle.toString());
-			}else {
-				System.out.println(arrayAttributs);
-				decomposition.ajouter(arrayAttributs.toString());
 			}
-			
+			System.out.println(arrayAttributs);
+			decomposition.ajouter(SQL.ATTRIBUT);
+			decomposition.ajouter(arrayAttributs.toString());
+
+
 			System.out.println(decomposition);
 			System.out.println("apres analyse du from :");
 			analyseFrom(apresFrom, decomposition);
 			System.out.println(decomposition);
+
 			/*
 			 * il reste a associer les attributs a leurs table
 			 */
-			
-			
-			
-			
-			/*
-			 * 
-			 * while, on split, puis on recupere les operations, les comparaison, et les jointure
-			 */
+
 		}else {
 			/**
 			 * La requete n'est pas un SELECT, testons les autres possibilité
