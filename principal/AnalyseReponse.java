5f7920645697225105eeefd4fbd67e718da96f2f
abdel42 <32333972+abdel42@users.noreply.github.com>
2018-04-22 15:56:23 +0200

    Update AnalyseReponse.java

--- principal/AnalyseReponse.java
+++ principal/AnalyseReponse.java
@@ -211,14 +211,14 @@ public class AnalyseReponse extends Arbre{
 	}
 
 
-	
+
 	public void analyseWhere(String requete, Arbre arbre) {
 
-		requete=" t_zzzlev.elev=t_prof.prof AND t_en.pro=tgh.tf AND id<2";
-		System.out.println("(where) La requete analysée est : "+requete);
+		//requete=" t_zzzlev.elev=t_prof.prof AND t_en.pro=tgh.tf AND id<2";
+		System.out.println("(where) La requete analysÃ©e est : "+requete);
 		arbre.ajouter(SQL.WHERE);
 		ArrayList<String> jointure = new ArrayList<>();
-		
+
 		/*
 		 * on va d'abord chercher si il existe des jointures
 		 * donc de la forme XXX.XXX = XXX.XXX
@@ -329,7 +329,7 @@ public class AnalyseReponse extends Arbre{
 			}
 			requete = requete.replace(p,"");
 			System.out.println("etat de la requete : "+ requete);
-			
+
 			/*
 			 * 
 			 * si il ne reste que des AND, c'est fini
@@ -359,14 +359,14 @@ public class AnalyseReponse extends Arbre{
 		if(matcher.find()) {
 			//System.out.println("Il y a "+matcher.groupCount());
 			System.out.println("match");
-			System.out.println(matcher.group());
+			System.out.println("'test)"+matcher.group());
 			System.out.println(">> "+matcher.group(1));//mettre dans une variable
 			System.out.println("le contenue est : "+matcher.group(2));//dans une autre variables
 		}
 
 		//chercher regex dans variable 1
 		//creer arbre avec variable 1 et 2
-		System.out.println();
+		//System.out.println();
 		return arbre;
 	}
 
@@ -385,14 +385,15 @@ public class AnalyseReponse extends Arbre{
 	}
 	public Arbre decomposeArbre(String requete) {
 		Arbre decomposition = new Arbre();
-		requete = "SELECT t-eleve,alpha.t-prof,COUNT(rotie)      \n FROM TABLE,eleve WHERE eleve = 2 AND prof=7 AND prof.nom=eleve.nom";// WHERE eleve = 2
+		System.out.println("La requete analysee par moi est "+requete);
+		//requete = "SELECT t-eleve,alpha.t-prof,COUNT(rotie)      \n FROM TABLE,eleve WHERE eleve = 2 AND prof=7 AND prof.nom=eleve.nom";// WHERE eleve = 2
 		requete = filtre(requete);
-		System.out.println("La requete analysée est : "+requete);
+		System.out.println("La requete analysÃ©e est : "+requete);
 
 		pattern = Pattern.compile("SELECT (.+)FROM (.+)");
 		matcher = pattern.matcher(requete);
 		if(matcher.find()) {
-			System.out.println("SELECT détecter");
+			System.out.println("SELECT dÃ©tecter");
 			/**
 			 * La requete commence donc par un SELECT
 			 * On verifie si il existe une fonction d'agregats
@@ -408,7 +409,7 @@ public class AnalyseReponse extends Arbre{
 			ArrayList<String> arrayAttributs = new ArrayList<>();
 			System.out.println("Les differents attibuts :");
 			for(int i=0;i<tabAttribut.length;i++) {
-				System.out.println(tabAttribut[i]);
+				System.out.println("()tadAtt "+tabAttribut[i]);
 			}
 			for(int i=0;i<tabAttribut.length;i++) {
 				arrayAttributs.add(tabAttribut[i]);
@@ -424,7 +425,7 @@ public class AnalyseReponse extends Arbre{
 				//une liste de mot cle type SUM, COUNT ... et une liste d'attibut
 				tabMotCleExiste = true;
 				for(int i=0;i<arrayAttributs.size();i++) {
-					System.out.println(arrayAttributs.get(i)+" est analysée");
+					System.out.println(arrayAttributs.get(i)+" est analysÃ©e");
 					if(distinct(arrayAttributs.get(i)) || agregat(" "+arrayAttributs.get(i))){//on ajoute un espace 
 						System.out.println("c'est un agregats");
 						tabMotCle.add(arrayAttributs.get(i));
@@ -471,7 +472,7 @@ public class AnalyseReponse extends Arbre{
 
 		}else {
 			/**
-			 * La requete n'est pas un SELECT, testons les autres possibilité
+			 * La requete n'est pas un SELECT, testons les autres possibilitÃ©
 			 */
 			pattern = Pattern.compile("INSERT INTO (.+)");
 			matcher = pattern.matcher(requete);
@@ -505,53 +506,169 @@ public class AnalyseReponse extends Arbre{
 		}
 		return decomposition;
 	}
-	
-	public void compareReponse(String requeteEleve, String requeteProf) {
+
+	public boolean compareReponse(String requeteEleve, String requeteProf) {
+
+		boolean resultatIdentique=true;;
 		/*
-		if(estSelect(requeteProf) && estSelect(requeteEleve)) {
-			try {
-				ResultSet rsEleve = Test.bdd.reqSQL(requeteEleve, 's');
-				ResultSet rsProf = Test.bdd.reqSQL(requeteProf, 's');
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				//e.printStackTrace();
-				Test.ie.ecrireErreur("Erreur SQL !");
-			}
-			
-		}else {
-			Test.ie.ecrireErreur("Ce ne sont pas des SELECT !");
-			return;
-		}
-		Test.ie.ecrireEnonce("Ce sont bien des SELECT");
-*/
-		
+		 * 
+		 * comparaison des resultats
+		 * 
+		 */
+		System.out.println("La requete du prof est :: "+requeteProf+" et celle de l'eleve est "+requeteEleve);
 		Arbre arbreProf = decomposeArbre(requeteProf);
 		Arbre arbreEleve = decomposeArbre(requeteEleve);
-		Arbre actuelProf;
-		Arbre actuelEleve;
-		String filsProf;
-		String filsEleve;
-		
-		switch(arbreProf.getElement()) {
+		System.out.println("<<<<<<<<<>>>>>>>>>>");
+		ArrayList<String> arrayProf = arbre2array(arbreProf);
+		ArrayList<String> arrayEleve = arbre2array(arbreEleve);
+
+		if(arrayEleve.equals(arrayProf)) {
+			System.out.println("Les deux sont strictement equivalent !");
+			System.out.println(arrayEleve);
+			System.out.println(arrayProf);
+			return true;
+		}
+		System.out.println(arbreProf);
+		int curseurProf,curseurEleve;
+		String temp1,temp2;
+		switch(arrayProf.get(0)) {
 		case(SQL.SELECT):
-			if(arbreEleve.getElement()!=SQL.SELECT) {
+			if(arrayEleve.get(0)!=SQL.SELECT) {
 				Test.ie.ecrireErreur("Mauvais type de requete !");
-				return;
+				return false;
 			}
 		/*
 		 * si on est la c'est que les 2 requete sont des select
 		 */
-		Test.ie.ecrireErreur("ok type de requete !");
+		//Test.ie.ecrireErreur("ok type de requete !");
+		curseurProf=1;
+		curseurEleve=1;
+		boolean terminer = false;
+		while(!terminer || curseurProf<arrayProf.size() || curseurEleve<arrayEleve.size()) {
+			System.out.println("La taille de arrayProf est "+arrayProf.size()+" la taille de eleve est "+arrayEleve.size()+"et curseur sont"
+					+ " pcurseur prof : "+curseurProf+" curseur eleve"+curseurEleve);
+			System.out.println("salut toi "+arrayProf.get(curseurProf));
+
+			if(curseurEleve >= arrayEleve.size() && curseurProf>= arrayProf.size()) {
+				return true;
+			}
+			switch (arrayProf.get(curseurProf)) {
+			case SQL.AGREGAT:
+				curseurProf++;
+				if(arrayEleve.get(curseurEleve)!=SQL.AGREGAT) {
+					Test.ie.ecrireErreur("Erreur dans le SELECT => il manque des AGRAGATS");
+					return false;
+				}
+				curseurEleve++;
+				/*
+				 * il y a un agregats dans les 2 requetes
+				 */
+				/*
+				 * on regarde si ce sont les meme agregats
+				 * on enleve tout les espaces, les crochet et on les compare
+				 */
+
+				temp1=filtreAgregat(arrayProf.get(curseurProf));
+				temp2=filtreAgregat(arrayEleve.get(curseurEleve));
+				if(!temp1.equals(temp2)) {
+					Test.ie.ecrireErreur("Erreur dans le SELECT ==> agragats incorrect");
+					return false;
+				}
+				curseurEleve++;
+				curseurProf++;
+				break;
+
+			case SQL.ATTRIBUT:
+
+				if(!arrayEleve.get(curseurEleve).equals(SQL.ATTRIBUT)) {
+					Test.ie.ecrireErreur("Erreur dans le SELECT ==> il manque un(des) attribut(s) !");
+					return false;
+				}
+				curseurEleve++;
+				curseurProf++;
+				temp1=filtreAgregat(arrayProf.get(curseurProf));
+				temp2=filtreAgregat(arrayEleve.get(curseurEleve));
+				if(!arrayEleve.get(curseurEleve).equals(arrayProf.get(curseurProf))) {
+					Test.ie.ecrireErreur("Erreur dans le SELECT ==> mauvais attributs !");
+					return false;
+					/*
+					 * 
+					 * on peut comparer la taille pour etre plus precis
+					 */
+				}
+				System.out.println("L'element prof traite est : "+arrayProf.get(curseurProf));
+				curseurEleve++;
+				curseurProf++;
+
+				/*
+				 * 
+				 * il faut maintenant traiter les attributs
+				 */
+				System.out.println("L'element prof traite est : "+arrayProf.get(curseurProf));
+				break;
+
+			case SQL.FROM:
+				if(!arrayEleve.get(curseurEleve).equals(SQL.FROM)) {
+					Test.ie.ecrireErreur("Erreur dans le FROM ==> il manque le FROM");
+					return false;
+				}
+				curseurEleve++;
+				curseurProf++;
+				temp1=filtreAgregat(arrayProf.get(curseurProf));
+				temp2=filtreAgregat(arrayEleve.get(curseurEleve));
+				if(!temp1.equals(temp2)) {
+					Test.ie.ecrireErreur("Erreur dans le FROM ==> table erronne");
+					return false;
+					/*
+					 * 
+					 * on peut comparer la taille pour savoir si il en manque ...
+					 */
+				}
+				/*
+				 * ON COMPARe LES TABLES
+				 */
+
+				temp1=filtreAgregat(arrayEleve.get(curseurEleve));
+				temp2=filtreAgregat(arrayProf.get(curseurProf));
+				if(!temp1.equals(temp2)) {
+					Test.ie.ecrireErreur("Erreur FROM : ce ne sont pas les bonnes tables");
+					return false;
+				}
+				curseurEleve++;
+				curseurProf++;
+				break;
+
+			default:
+				//Test.ie.ecrireErreur("On est dans la place");
+
+				if(resultatIdentique) {
+					return true;
+				}
+
+			}
 		}
-		
-	}
-	
-	
-	public String filsSuivant(String filsActuel) {
-		if(filsActuel==SQL.FILS_DROIT) {
-			return SQL.FILS_GAUCHE;
 		}
-		return SQL.FILS_DROIT;
+		Test.ie.ecrireErreur("OK");
+		return true;
 	}
-	
+
+	public String filtreAgregat(String filte) {
+		String beta;
+		beta = filte.replaceAll("[|]| ", "");
+		return beta;
+	}
+	public ArrayList<String> arbre2array(Arbre arbre){
+		ArrayList<String> array = new ArrayList<>();
+		Arbre courant;
+		array.add(arbre.getElement());
+		courant=arbre;
+		while(courant.getDroite()!=null && courant.getGauche() != null) {
+			array.add(courant.getGauche().getElement());
+			array.add(courant.getDroite().getElement());
+			courant=courant.getDroite();
+		}
+
+		return array;
+	}
+
 }
