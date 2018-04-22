8c8ea184d5c9581acc1db13d31f5d72df69a3329
abdel42 <32333972+abdel42@users.noreply.github.com>
2018-04-22 15:57:14 +0200

    Update Test.java

--- principal/Test.java
+++ principal/Test.java
@@ -20,16 +20,16 @@ import javax.swing.JFrame;
 public class Test {
 	public static InterfaceEtudiant ie ;
 	public static GestionBDD bdd;
-	
-	
-	
+
+
+
 	public static void main(String[] args) throws SQLException, InterruptedException, ScriptException {
 		// TODO Auto-generated method stub
 		ie = new InterfaceEtudiant();
-        ie.setVisible(true);
-        bdd = new GestionBDD();
-        AnalyseReponse analyseR = new AnalyseReponse();
-        analyseR.compareReponse("SELECT g", "SELECT gr");
+		ie.setVisible(true);
+		bdd = new GestionBDD();
+		AnalyseReponse analyseR = new AnalyseReponse();
+		//analyseR.compareReponse("SELECT g", "SELECT gr");
 		/*
 		Arbre a = new Arbre();
 		Arbre droit = new Arbre("fils droit", null, null);
@@ -38,21 +38,21 @@ public class Test {
 		a.setGauche(gauche);
 		a.setElement("test");
 		System.out.println(a.toString());
-		*/
-		
-		
-		analyseR.decomposeArbre("");
+		 */
+
+
+		//Arbre test4 = analyseR.decomposeArbre("");
 		//System.out.println(analyseR.agregat("SELECT AVG     (televe, jena"));
 		//analyseR.analyseFrom("t_eleve , t,prof");
 		boolean b = analyseR.opComparaison("SELECT nom_colonnes FROM nom_table WHERE condition1 A ND (condition2 O R condition3)");
 		System.out.println(b);
 		//analyseR.analyseAgregat("");
-		
-		
+
+
 		Pattern pattern;
 		Matcher matcher;
 		String requete = "ele<15 AND eleve.ter = 52 AND elve_y.nom=prof.nom AND ele.ef=fg.gt";
-		//pattern = Pattern.compile("((.+)[.](.+)[ ]*=[ ]*(.+¨ )[.](.+))");
+		//pattern = Pattern.compile("((.+)[.](.+)[ ]*=[ ]*(.+Â¨ )[.](.+))");
 		pattern = Pattern.compile("([a-zA-Z0-9_-]+)[.]([a-zA-Z0-9_-]+)=([a-zA-Z0-9_-]+)[.]([a-zA-Z0-9_-]+)");
 		matcher = pattern.matcher(requete);
 		if(matcher.find()) {
@@ -65,21 +65,37 @@ public class Test {
 			requete = requete.replace(test, "");
 			System.out.println(requete);
 		}
-		
-		
+
+
 		analyseR.analyseWhere("", new Arbre());
-		
+
 		Exercice exercice = new Exercice("test");
 		exercice.getTitre();
 		Question actuelle;
-		
-       // java.awt.EventQueue.invokeLater(new Runnable() {
-          //  public void run() {
-                
-           // }
-       // });
-              
 
+		//System.out.println(test4.toString());
+	//	System.out.println("");
+//		System.out.println(analyseR.arbre2array(test4).toString());
+
+		String alpha = "[COUNT( ROTIE )]";
+		String beta = "[COUNT(ROTIE)]";
+		alpha = alpha.replaceAll("[|]| ", "");
+		beta = beta.replaceAll("[|]| ", "");
+		System.out.println(alpha.contentEquals(beta));
+		System.out.println(alpha+"   "+beta);
+		
+		try {
+			boolean essai = analyseR.compareReponse("SELECT eleve FROM prof WHERE 1<id AND hid.fr=qr.seg ", "SELECT eleve FROM PROF WHERE id.vf=fv.se");
+		System.out.println(essai);
+		}catch(java.lang.IndexOutOfBoundsException e) {
+			System.out.println("Une erreur s'est produite...");
+			/*
+			 * 
+			 * erreur IndexOutOfBound
+			 */
+		}
+		
+		//System.out.println(essai);
 	}
 
 }
