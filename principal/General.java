007a6a32763dac0949b4cb6c7529fabcc672b215
abdel42 <32333972+abdel42@users.noreply.github.com>
2018-11-15 10:59:17 +0100

    Update General.java

--- principal/General.java
+++ principal/General.java
@@ -4,8 +4,7 @@ package projetTutore;
 
 public interface General {
 	
-	//public String mdp="4ag2ncarnot";
-	//public String identifiant="ba02996q";
+	
 	
 	public String mdp = "4ag2ncarnot";
 	public String identifiant = "root";
